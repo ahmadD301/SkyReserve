7c7712c5e10b994fae215788b650f1f170cd7c4b
Ahmad Dardouk <alaaedeen84@gmail.com>
2026-03-03 21:26:17 +0200

    add Passenger part to the system

--- Domain/Entities/Passenger.cs
+++ Domain/Entities/Passenger.cs
@@ -1,1 +1,40 @@
+using System.Security.Cryptography.X509Certificates;
 
+public class Passenger
+{
+    public Guid Id { get; }
+    public string FirsName { get; private set; }
+    public string LastName { get; private set; }
+    public string? PassportNumber { get; private set; }
+    public DateTime? DateOfBirth { get; private set; }
+
+    public Passenger(string firstName, string lastName, string? passportNumber = null, DateTime? dateOfBirth = null)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+        {
+            throw new ArgumentException("First name cannot be null or empty.", nameof(firstName));
+        }
+        if (string.IsNullOrWhiteSpace(lastName))
+        {
+            throw new ArgumentException("Last name cannot be null or empty.", nameof(lastName));
+        }
+
+        Id = Guid.NewGuid();
+        FirsName = firstName;
+        LastName = lastName;
+        PassportNumber = passportNumber;
+        DateOfBirth = dateOfBirth;
+    }
+    public void UpdatePassportNumber(string passportNumber)
+    {
+        if (string.IsNullOrWhiteSpace(passportNumber))
+        {
+            throw new ArgumentException("Passport number cannot be null or empty.", nameof(passportNumber));
+        }
+        PassportNumber = passportNumber;
+    }
+    public string GetFullName()
+    {
+        return $"{FirsName} {LastName}";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,4 @@
-﻿
-using Domain.Enums;
-
-var flight = new Flight("AA123", "New York", "Los Angeles", DateTime.Now.AddHours(5));
-
-flight.AddSeat(new Seat("1A", SeatClass.Economy));
-flight.AddSeat(new Seat("1B", SeatClass.Economy));
-flight.AddSeat(new Seat("1C", SeatClass.Business));
-Console.WriteLine($"Flight {flight.FlightNumber} from {flight.Origin} to {flight.Destination} departs at {flight.DepartureTime}");
-
-IEnumerable<Seat> availableSeats = flight.Seats.Where(s => s.IsBooked == false);
-Console.WriteLine("Available seats:");
-foreach (var seat in availableSeats)
-{
-    Console.WriteLine($" - {seat.SeatNumber} ({seat.SeatClass})");
-}
-
-var seat1 = flight.GetSeat("1A");
-seat1.Book();
-Console.WriteLine($"Seat {seat1.SeatNumber} is now booked.");
-
-Console.WriteLine("Available seats:");
-foreach (var seat in availableSeats)
-{
-    Console.WriteLine($" - {seat.SeatNumber} ({seat.SeatClass})");
-}
-
+﻿var passenger = new Passenger("John", "Smith");
+System.Console.WriteLine(passenger.GetFullName());
+passenger.UpdatePassportNumber("123456789");
+System.Console.WriteLine($"Passenger {passenger.GetFullName()} has passport number: {passenger.PassportNumber}");
